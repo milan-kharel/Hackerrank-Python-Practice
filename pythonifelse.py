4c08c6e0e16760e4eafcc6c97f95af5efa9244db
milan-kharel <milan.kharel120@gmail.com>
2024-03-04 15:50:34 +0545

    first program

---
+++ pythonifelse.py
@@ -0,0 +1,13 @@
+n = 5
+    
+if ((n%2)==0 and 20<n)or ((n%2)==0 and 2<=n<=5):
+    print("Not Weird")
+elif ((n%2)==1)or ((n%2)==0 and 6<=n<=20):
+    print("Weird")
+        
+# Small code 
+# result = "Not Weird" if (n % 2 == 0 and (20 < n or (2 <= n <= 5))) else "Weird"
+# print(result)
+
+    
+
